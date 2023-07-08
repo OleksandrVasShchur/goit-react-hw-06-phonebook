512eea00f6b1712186d05e22285ba2281c955e90
Oleksandr Shchur <oleksandrschur7@gmail.com>
2023-07-08 18:43:26 +0300

    finish

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const startState = [];
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: startState,
+
+  reducers: {
+    addContactsSlice(state, action) {
+      state.push(action.payload);
+      console.log(state, action);
+    },
+
+    deleteContacts(state, action) {
+      return state.filter(contact => contact.id !== action.payload);
+    },
+  },
+});
+
+// Генератори екшенів
+export const { addContactsSlice, deleteContacts } = contactsSlice.actions;
+// Редюсер слайсу
+export const contactsReducer = contactsSlice.reducer;
